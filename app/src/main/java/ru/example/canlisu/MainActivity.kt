1cb2c2c93c95eea2d24871c711360506243bf7d9
R-STYL3R <tor.vipo@gmail.com>
2025-08-16 19:56:22 +0400

    fixing bug

--- app/src/main/java/ru/example/canlisu/MainActivity.kt
+++ app/src/main/java/ru/example/canlisu/MainActivity.kt
@@ -23,35 +23,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navController = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment_content_main)
+            .findFragmentById(R.id.nav_home)
             ?.findNavController()
             ?: error("NavHostFragment not found")
-
-        // Скрывать нижнюю навигацию на экране логина
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            binding.bottomNav.isVisible = destination.id != R.id.loginFragment
-        }
-
-        // Если используешь флаг «залогинен» в DataStore — раскомментируй блок ниже
-        /*
-        lifecycleScope.launch {
-            AuthPrefs.isLoggedIn(this@MainActivity).collectLatest { loggedIn ->
-                if (loggedIn && navController.currentDestination?.id == R.id.loginFragment) {
-                    val options = androidx.navigation.NavOptions.Builder()
-                        .setPopUpTo(navController.graph.startDestinationId, true)
-                        .build()
-                    navController.navigate(R.id.fragment_home, null, options)
-                }
-            }
-        }
-        */
 
         binding.bottomNav.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         // Без AppBarConfiguration:
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navController = findNavController(R.id.bottomNav)
         return navController.navigateUp()
     }
 }
